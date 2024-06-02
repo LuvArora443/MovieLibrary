ac903713fe8b698b2d40661404bd67d8a4d566cc
Luv Arora <139987286+LuvArora443@users.noreply.github.com>
2024-06-02 22:50:09 +0530

    Update theme.js

--- theme.js
+++ theme.js
@@ -1,3 +1,26 @@
+// import { extendTheme } from "@chakra-ui/react";
+// import { mode } from "@chakra-ui/theme-tools";
+
+// const config = {
+//   initialColorMode: "dark",
+//   useSystemColorMode: false,
+// };
+
+// const styles = {
+//   global: (props) => ({
+//     body: {
+//       bg: mode(
+//         props.theme.semanticTokens.colors["chakra-body-bg"]._light,
+//         "blackAlpha.900"
+//       )(props),
+//     },
+//   }),
+// };
+
+// const theme = extendTheme({ config, styles });
+
+// export default theme;
+
 import { extendTheme } from "@chakra-ui/react";
 import { mode } from "@chakra-ui/theme-tools";
 
@@ -9,10 +32,8 @@ const config = {
 const styles = {
   global: (props) => ({
     body: {
-      bg: mode(
-        props.theme.semanticTokens.colors["chakra-body-bg"]._light,
-        "blackAlpha.900"
-      )(props),
+      bg: mode("beige.100", "beige.900")(props), // Change background color to beige in dark mode
+      color: mode("black", "white")(props), // Adjust text color for better readability
     },
   }),
 };
